8537181685c68427b87b0c5b5851c360618d9ec0
salah <salah@email.com>
2023-07-24 04:39:25 -0700

    done task

--- 0-_printf.c
+++ 0-_printf.c
@@ -1,56 +1,68 @@
 #include "main.h"
 
+/**
+*_printf - that produces output according to a format
+*@format: requested format
+*Return: 0 for always sucess
+*/
+
 int _printf(const char *format, ...)
 {
-    int i = 0, j = 0;
-    char *s;
+	if (!format || (format[0] == '%' && !format[1]))
+	{
+		return (-1);
+	}
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	{
+		return (-1);
+	}
+	myloop(format);
+	return (0);
+}
 
-    va_list list;
-    va_start(list, format);
+/**
+*myloop - to avoid error morethan 40line in a function
+*@format: from printf
+*/
 
-    if (!format || (format[0] == '%' && !format[1]))
-    {
-        return -1;
-    }
-    if (format[0] == '%' && format[1] == ' ' && !format[2])
-    {
-        return -1;
-    }
+void myloop(const char *f, ...)
+{
+	int i = 0, j = 0;
+	char *s;
+	va_list list;
 
-    while (format[i])
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            switch (format[i])
-            {
-                case 'c':
-                    _putchar(va_arg(list, int));
-                    break;
-                case 's':
-                    s = va_arg(list, char *);
-                    while (s[j] != '\0')
-                    {
-                        _putchar(s[j]);
-                        j++;
-                    }
-                    break;
-                case '%':
-                    _putchar('%');
-                    break;
-                default:
-                    _putchar('?');
-                    break;
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-        }
-
-        i++;
-    }
-
-    va_end(list);
-    return 0;
+	va_start(list, f);
+	while (f[i])
+	{
+		if (f[i] == '%')
+		{
+			i++;
+			switch (f[i])
+			{
+				case 'c':
+					_putchar(va_arg(list, int));
+					break;
+				case 's':
+					s = va_arg(list, char *);
+					while (s[j] != '\0')
+					{
+						_putchar(s[j]);
+						j++;
+					}
+					break;
+				case '%':
+					_putchar('%');
+					break;
+				default:
+					i++;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(f[i]);
+		}
+		i++;
+	}
+	va_end(list);
 }
