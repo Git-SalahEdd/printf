700183b4ff5855ce7027b5df3fca934fd9cb7c9c
salah <salah@email.com>
2023-07-25 07:34:11 -0700

    done task

--- main.h
+++ main.h
@@ -2,13 +2,6 @@
 #define MAIN_H
 #define NULL_STRING "(null)"
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <limits.h>
-#include <unistd.h>
-
 int _putchar(char c);
 int _printf(const char *format, ...);
 
