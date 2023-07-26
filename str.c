a88d685e53bde2ef8129c99065f09bc813799f6e
salah <salah@email.com>
2023-07-25 17:03:25 -0700

    done task

--- str.c
+++ str.c
@@ -8,12 +8,13 @@
 
 int str(const char *s)
 {
-        int i = 0;
-        while (*s)
-        {
-                _putchar(*s);
-                s++;
-                i++;
-        }
-        return (i);
+	int i = 0;
+
+	while (*s)
+	{
+		_putchar(*s);
+		s++;
+		i++;
+	}
+	return (i);
 }
