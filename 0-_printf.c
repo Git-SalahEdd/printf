f7dcd6a43e7a1d6dad48e738039cbd0c7160fdd9
AdolfMkonda <adolfanatory@gmail.com>
2023-07-26 05:21:10 +0300

    handling b conversion specifier

--- 0-_printf.c
+++ 0-_printf.c
@@ -22,7 +22,7 @@ int chk(const char *format)
 
 int _printf(const char *format, ...)
 {
-	int i = 0;
+	int i = 0, num, num1, temp, count;
 	const char *s;
 	va_list list;
 
@@ -47,10 +47,29 @@ int _printf(const char *format, ...)
 					break;
 				case 'd':
 				case 'i':
-					i += ptr_num(va_arg(list, int));
+					num = va_arg(list, int);
+					if (num < 0)
+					{ i += _putchar('-');
+						num = -num; }
+					temp = num;
+					count = 1;
+					while (temp / 10 > 0)
+					{
+						temp /= 10;
+						count *= 10;
+					}
+					while (count > 0)
+					{
+						i += _putchar((num / count) % 10 + '0');
+						count /= 10;
+					}
 					break;
 				case '%':
 					i += _putchar('%');
+					break;
+				case 'b':
+					num1 = va_arg(list, int);
+					i += _print_binary(num1);
 					break;
 				default:
 					i += _putchar('%');
--- main.h
+++ main.h
@@ -11,5 +11,7 @@ int chk(const char *format);
 int str(const char *s);
 int ptr_num(int num);
 int ptr_num2(int num);
+int  str(const char *s);
+int _print_binary(int num);
 
 #endif
