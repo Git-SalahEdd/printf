8720a209a0fb5217112a3f0bbd761fa8fddc28db
salah <salah@email.com>
2023-07-28 11:24:51 -0700

    extracted function for print number in separated file to reduce code lines check files pt_num.c and pt_num2.c

--- 0-_printf.c
+++ 0-_printf.c
@@ -22,7 +22,7 @@ int chk(const char *format)
 
 int _printf(const char *format, ...)
 {
-	int i = 0, temp, num, count;
+	int i = 0;
 	const char *s;
 	va_list list;
 
@@ -46,10 +46,10 @@ int _printf(const char *format, ...)
 				i += str(s);
 				break;
 			case 'd':
-				i += pt_num2(va_arg(list, int));
+				i += ptr_num2(va_arg(list, int));
 				break;
 			case 'i':
-				i += pt_num(va_arg(list, int));
+				i += ptr_num(va_arg(list, int));
 				break;
 			case '%':
 				i += _putchar('%');
