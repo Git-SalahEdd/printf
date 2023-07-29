93cffbd6dd19b1d8c0004ac8db3195289955fa78
salah <salah@email.com>
2023-07-28 17:21:44 -0700

    done task

--- main.c
+++ main.c
@@ -12,11 +12,11 @@ int main(void)
 	/**int i = 0;
 	int len1;
 	char *s = "AAAAAAAA";*/
-
-	/**_printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
-	_printf("%i\n", INT_MIN);*/
+	
+	_printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+	_printf("%i\n", INT_MIN);
 	_printf("%d\n", INT_MIN);
-	/**_printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));*/
+	_printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
 
 	/**len1 = _printf("Let's try to printf a %d simple sentence.\n", i);
 	printf("%d" ,len1);
--- pt_num2.c
+++ pt_num2.c
@@ -8,18 +8,39 @@
 
 int ptr_num2(int num)
 {
-	int num_d = num;
-	int z = 0;
-
-	if (num_d < 0)
+	int z = 0, i;
+	
+	if (num == INT_MAX)
+	{
+		const char maxIntStr[] = "2147483647";
+		for (i = 0; maxIntStr[i] != '\0'; i++)
+		{
+			z += _putchar(maxIntStr[i]);
+		}
+		return (z);
+	}
+	if (num == INT_MIN)
+	{
+		const char minIntStr[] = "-2147483648";
+		for (i = 0; minIntStr[i] != '\0'; i++)
+		{
+			z += _putchar(minIntStr[i]);
+		}
+		return (z);
+	}
+	if (num == 0)
+	{
+		z += _putchar('0');
+		return (z);
+	}
+	if (num < 0)
 	{
 		putchar('-');
-		num_d = num_d;
+		num = -num;
 		z++;
 	}
-	if (num_d / 10)
-		z += ptr_num2((unsigned int) num_d / 10);
-	putchar(((unsigned int) num_d % 10) + '0');
-	z++;
+	if (num / 10 != 0)
+		z += ptr_num2((unsigned int) num / 10);
+	z += putchar('0' + (num % 10));
 	return (z);
 }
